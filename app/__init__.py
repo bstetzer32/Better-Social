8d1a6c5d6e4761871f9a2caf0767116ac730ffa1
Ben Stetzer <benstetzer@gmail.com>
2021-09-21 17:03:21 -0400

    working on implementing sqla

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
+import os
+from .models import db
 from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from .config import Config
 
 app = Flask(__name__)
 
+db.init_app(app)
+Migrate(app, db)
+CORS(app)
+better_social = 'As1F#d8e0&'
